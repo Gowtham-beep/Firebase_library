b8064e7651993db113c152e064542d35e53726a7
Gowtham <your.email@example.com>
2024-11-01 21:04:23 -0700

    firebase app connected

---
+++ client/src/firebase.jsx
@@ -0,0 +1,11 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0sef8Foz6-Wx4WewPf9yQ6C1Sh46Q4uA",
+    authDomain: "bookstore-app-c93df.firebaseapp.com",
+    projectId: "bookstore-app-c93df",
+    storageBucket: "bookstore-app-c93df.firebasestorage.app",
+    messagingSenderId: "317169085253",
+    appId: "1:317169085253:web:e8bb905192f841b06dec2c"
+  };
+const app=initializeApp(firebaseConfig)  
